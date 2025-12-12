118432fd6e315baa3badd9e9d2b6a1bd0e691d20
jefin-s <jefin.s786@gmail.com>
2025-12-12 14:49:44 +0530

    Add project files.

---
+++ Arithmetic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Arithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+
+            Console.WriteLine($"Sum: {a + b}");
+            Console.WriteLine($"Difference: {b - a}");
+            Console.WriteLine($"Product: {a * b}");
+            Console.WriteLine($"division: {a / b}");
+            Console.WriteLine($"modulus:{a % b}");
+
+
+
+        }
+    }
+}
